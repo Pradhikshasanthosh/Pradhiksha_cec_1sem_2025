144076f5f1e330a856783f77720e10c81ac837ea
Pradhiksha <pradhikshasanthosh@gmail.com>
2025-11-20 15:39:06 +0530

    day4 code pushed

---
+++ Day4/1_secprg.c
@@ -0,0 +1,10 @@
+#include<conio.h>
+#include<stdio.h>
+int a=10,b=5:
+int sum;
+int main()
+{
+    sum=a+b;
+    printf{"sum of %d and %d is :%d/n",a,b,sum};
+    return 0;
+}
---
+++ Day4/2_variable.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+   int age =30;
+   printf("your age is :%d",age);
+   float marks = 23.44;
+   printf("\nyour age is :%f", marks);
+   double pi=3.14;
+   printf("\nvalue of pi is :%lf", pi);
+   char gender = 'F';
+   printf("\nyour gender is :%c", gender);
+
+}
